f3c12dd08931bed8577a74a9ad71fcb375a1011f
DaBlueOne <DaBlueOne@users.noreply.github.com>
2016-10-25 13:42:02 -0400

    Removed whitespace

--- Main.java
+++ Main.java
@@ -9,17 +9,12 @@ public class Main
 	{
 		
 		String test1 = "HI!";
-		
 		System.out.println("Unencrypted text: " + test1);
-		
 		String test2 = "HI!";
 		String test3 = BCrypt.gensalt(10);
 		String test4 = BCrypt.hashpw(test2, test3);
-		
 		System.out.println("Salt: " + test3 + "\n" + "Hashed: " + test4);
-		
 		boolean test5 = BCrypt.checkpw(test2, test4);
-		
 		boolean test6 = BCrypt.checkpw(test2, test3);
 		
 		if(test5)
@@ -34,8 +29,5 @@ public class Main
 			System.out.println("CORRECT!");
 		}else if(!test6)
 			System.out.println("NOPE..");
-		
-		
 	}
-
 }
